1bd3d9480b699b96a6c3c3325595f4d1a4d4a341
nikhil kumar shukla <143585837+Nikhil6306@users.noreply.github.com>
2024-10-11 14:14:30 +0530

    loop.js

---
+++ loop.js
@@ -0,0 +1,128 @@
+//let num=7;
+//switch(num) {
+//       case 1: console.log('a');
+//       break;
+//       case 2: console.log('b');
+//       break;
+//       case 3: console.log('c');
+//       break;
+//       case 4: console.log('d');
+//       break;
+//       case 5: console.log('e');
+//       break;
+//       default:console.log('f');
+//}
+//
+//
+//
+//for (let step = 0; step < 5; step++) {
+//       // Runs 5 times, with values of step 0 through 4.
+//       console.log("Walking east one step");
+//     }
+//
+//for (let i=1; i<=10; i=i+1)
+//
+//for( let i=1; i<=5; i++){
+//       console.log(i);
+//}
+//
+//for( let i=10; i>=0; i--){
+//       console.log(i);
+//}
+
+
+
+//for (i=0; i<=6; i++)
+//{
+//       if(i==4)
+//              break;
+//       else
+//       console.log(i);
+//}
+
+     // do while loop
+
+//let i=0;
+//do{
+//       if(i==4)
+//              break;
+//       else
+//       console.log(i);
+//       i++;
+//}while(i<=6);
+//
+
+      //for statement.
+
+//for( let i=1; i<=7; i++)
+//{
+//       if(i==4)
+//              continue;
+//       else
+//       console.log(i);
+//}
+//
+//
+//     //while loop
+//
+//let i=1;
+//while(i<7){
+//       if(i==4){
+//              i++;
+//              continue;
+//       }
+//       else
+//       console.log(i);
+//       i++;
+//}
+//
+
+    // for loop
+
+//for(i=1; i<=10; i++){
+//       console.log(i);
+//}
+
+      // do while loop
+
+
+//let i=1;
+//do{
+//       console.log("nikhil");
+//       i++;
+//} while(i<=10);
+
+
+
+
+      // string
+
+//let firstname="nikhil shukla"
+//let lastname="shukla"
+//
+//console.log(firstname);
+
+
+
+
+//let opt1="english";
+//let opt2="hindi";
+//
+//console.log(opt2.toUpperCase());
+
+//let finals =opt1 + opt2;
+//console.log(finals)
+
+
+
+
+//let str="nikhil";
+//console.log(str.substring(0));
+//console.log(str.substring(0,3));
+//console.log(str.toUpperCase());
+
+
+
+let sent="hello nikhil shukla web";
+let words=sent.split(' ');
+console.log(words);
