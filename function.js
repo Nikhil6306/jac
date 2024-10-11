a1f56caf6c1e1b00eba4f302fcda6e9eb95c5226
nikhil kumar shukla <143585837+Nikhil6306@users.noreply.github.com>
2024-10-11 14:13:30 +0530

    function.js

---
+++ function.js
@@ -0,0 +1,47 @@
+
+//// fuction define
+//function myname(){
+//console.log("love babbar");
+//}
+//
+////function use-function call
+//saymyname();
+
+//function printcounting() {
+//       for(i=1; i<=100; i++) {
+//              console.log(i);
+//       }
+//}
+//
+//printcounting();
+//
+//function printnumber (num) {
+//       console.log("printnig number: ", num);
+//}
+//printnumber(100);
+//
+ 
+
+
+    //return function
+
+// function getsum(a,b,c) {
+//       let sum=a+b+c;
+//       return sum;
+//       //unreachable statement
+//      // let a=10;
+//      // let b=10;
+//      // let sum=a+b;
+//      // console.log(sum);
+// }
+//let ans= getsum(1,2,3);
+//console.log("printing sum:", ans);
+
+
+//       //arrow function
+//
+//   let getexp=(x,y) => {
+//       let ans=x**y;
+//       return ans;
+//   }
+//   console.log(getexp(2,10));
